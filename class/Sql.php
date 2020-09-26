1f5b097c602e90aecbdb114c10f79edcade31579
Contardi <antoniocontardi.junior@gmail.com>
2020-09-26 13:30:31 -0300

    inserir e alterar

--- class/Sql.php
+++ class/Sql.php
@@ -49,4 +49,4 @@ class Sql extends PDO {
 
 }
 
-?>
+ ?>
--- index.php
+++ index.php
@@ -1,12 +1,18 @@
 <?php
 
 require_once("config.php");
-
+/*
+//Carega 1 usuario
 $usuario = new Usuario();
-
 $usuario->loadById(3);
-
 echo $usuario;
+*/
+
+/*
+//Carrega um lista de usuarios
+$lista = Usuario::getList();
+echo json_encode($lista);
+*/
 
 /*
 $sql = new Sql();
@@ -15,4 +21,32 @@ $usuarios = $sql->select("SELECT * FROM tb_usuarios");
 
 echo json_encode($usuarios);
 */
+
+/*
+//Carrega um lista de usuarios buscando pelo login
+$search = Usuario::search("ro");
+echo json_encode($search);
+*/
+
+/*
+//Carrega um usuario usando o login e a senha
+$usuario = new Usuario();
+$usuario->login("root", "!@#$");
+*/
+
+/*
+//Criando um novo usuario
+$aluno = new Usuario("aluno", "@luno0");
+$aluno->insert();
+echo $aluno;
+*/
+
+$usuario = new Usuario();
+
+$usuario->loadById(8);
+
+$usuario->update("professor", "!@#$%¨&*");
+
+echo $usuario;
+
 ?>
